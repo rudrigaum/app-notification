c7198644d71216952fe24e91d5f35888aa887f4c
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2021-03-01 12:08:18 -0300

    Created Layout, Class and Method

---
+++ app/src/main/java/com/bootcampeveriskotlin/notificationapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.bootcampeveriskotlin.notificationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener {
+            this.showNotification("1234", "bootcamp Android", "Kotlin Android Everis")
+        }
+    }
+}
